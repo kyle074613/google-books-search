718d4c2caf80542cd0c109164becd8970c519eb1
kyle074613 <kylemj@uci.edu>
2020-04-22 23:22:02 -0700

    removed unnecessary code

--- server.js
+++ server.js
@@ -7,15 +7,12 @@ const app = express();
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
-
-// Send every other request to the React app
-// Define any API routes before this runs
 app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks")
